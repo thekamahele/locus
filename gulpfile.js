37c89d7748b21f31d911d4dd810f4988e8007703
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-12-26 12:54:41 -0800

    (setup) Setting up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,59 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-gulp.task('concat', function() {
-    return gulp.src([
-        './public/app/*.js',
-        './public/controllers/*.js'
-    ])
-    .pipe(plugins.concat('all.js'), { newLine: ';' })
-    .pipe(gulp.dist('./dist/'))
+gulp.task('default', function() {
+
+});
+
+gulp.task('clean-styles', function() {
+
+});
+
+gulp.task('minify-css', function() {
+
+});
+
+gulp.task('lint', function() {
+
+});
+
+gulp.task('clean-scripts', function() {
+
+});
+
+gulp.task('combinejs', function() {
+	return gulp.src([
+			'./public/app/*.js',
+			'./public/controllers/*.js'
+		])
+		.pipe(plugins.concat('all.js'), { newLine: ';' })
+		.pipe(gulp.dist('dist/scripts'))
+		.pipe(plugins.uglify())
+		.pipe(gulp.dist('dist/scripts'))
 });
 
 gulp.task('browser-sync', function(){
-    plugins.browserSync.init({
-        server : {
-            baseDir: './'
-        }
-    })
+	plugins.browserSync.init({
+		server : {
+			baseDir: './'
+		}
+	})
 });
 
 gulp.task('watch', function () {
-    gulp.src([
-        './public/styles/*.css',
-        './public/app/*.js',
-        './public/app/controllers/*.js'
-    ],
-    [])
-    .pipe(plugin.watch([
-        './public/styles/*.css',
-        './public/app/*.js',
-        './public/app/controllers/*.js'
-    ]))
+	gulp.src([
+			'./public/styles/*.css',
+			'./public/app/*.js',
+			'./public/app/controllers/*.js'
+		],
+		[])
+		.pipe(plugin.watch([
+			'./public/styles/*.css',
+			'./public/app/*.js',
+			'./public/app/controllers/*.js'
+		]))
 })
+
+gulp.task('mocha', function() {
+
+});
