4b9c39f76c3016a0e52794fc3030c1d8c388175d
Andrew Deal <deal.e.andrew@gmail.com>
2015-12-26 11:52:48 -0800

    Adds /api/signn and /api/signout/ routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var passport = require('passport')
 var _ = require('lodash');
 var app = express();
 
@@ -10,8 +9,16 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
-//app.use(passport.initialize());
-//app.use(passport.session());
+
+app.route('/api/create')
+   .post(function(req, res, next) {
+
+   });
+
+app.route('/api/signin')
+   .post(function(req, res, next){
+
+   });
 
 
 
