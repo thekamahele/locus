7f4a568fbac6ad138793784be096bf8bc410496e
Andrew Deal <deal.e.andrew@gmail.com>
2015-12-26 11:59:13 -0800

    Adding cors support

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var methodOverride = require('method-override');
 var _ = require('lodash');
 var app = express();
 
 // Middleware
 app.use(express.static('public'));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
@@ -19,9 +21,6 @@ app.route('/api/signin')
    .post(function(req, res, next){
 
    });
-
-
-
 
 
 var server = app.listen(3000, function(){
