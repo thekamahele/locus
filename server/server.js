4aa1a098be053486935e8c3035a95577b024f34f
Andrew Deal <deal.e.andrew@gmail.com>
2015-12-26 17:48:03 -0800

    Adds POST route to /api/signin

--- server/server.js
+++ server/server.js
@@ -17,12 +17,23 @@ app.route('/api/create')
 
    });
 
-app.route('/api/signin')
-   .post(function(req, res, next){
-
-   });
+app.post('/api/signin', authenticate, function (req, res) {
+    res.send(req.body);
+});
 
 
 var server = app.listen(3000, function(){
   console.log("Server is running on http://localhost:" + server.address().port)
-})
+});
+
+//////////////////// Utilities /////////////////
+function authenticate ( res, req, next ) {
+  console.log('in authenticate')
+    var body = res.body;
+
+    if ( !body.username || !body.password ) {
+        res.status(400).end('Must provide username and password');
+    }
+
+    next();
+}
