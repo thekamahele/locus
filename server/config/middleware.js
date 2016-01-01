d6fac2b92892364032eaea9a68b0fef5add16527
Andrew Deal <deal.e.andrew@gmail.com>
2016-01-01 13:14:59 -0800

    Adds user router

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,21 +2,20 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var methodOverride = require('method-override');
-var jwt = require('jsonwebtoken');
-var expressJwt = require('express-jwt');
+
 
 module.exports = function ( app, express ) {
     var userRouter = express.Router();
 
     app.use(morgan('dev'));
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    //app.use(bodyParser.urlencoded({extended: true}));
+    //app.use(cors());
+    //app.use(methodOverride('X-HTTP-Method-Override'));
     app.use(express.static('public'));
-    app.use(cors());
-    app.use(methodOverride('X-HTTP-Method-Override'));
-    app.use(expressJwt({ secret : 'andrew' }).unless({ path : ['/api/signin', '/api/signup'] }));
 
     app.use('/api/users', userRouter);
 
+    require('../users/userRoutes.js')( userRouter );
 
-}
+};
