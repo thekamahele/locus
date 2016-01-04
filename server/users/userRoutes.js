82f5955e0c2752f17723999401ec2b41fb43ba87
Andrew Deal <deal.e.andrew@gmail.com>
2016-01-03 16:06:54 -0800

    Adds middleware to api/users/signup and signin

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,6 +3,7 @@ var userController = require('./userController');
 module.exports = function ( app ) {
 
     app.post('/signin', userController.signin);
-    app.post('/signup', userController.signup);
+    app.post('/signup', userController.signup, userController.createUser);
+    //app.post('/signout', userController.signout);
 
 };
