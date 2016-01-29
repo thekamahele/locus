ef433ef4ef22f6b479dcfa9c638dec48055d0d66
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2016-01-29 09:27:37 -0800

    (refactor) Refactored controllers and implemented auth functionality

--- public/app/controllers/auth.js
+++ public/app/controllers/auth.js
@@ -7,40 +7,41 @@
         var auth = this;
         auth.username = '';
         auth.password = '';
-        auth.name = '';
+        auth.firstName = '';
+        auth.lastName = '';
         auth.email = '';
         auth.login = login;
         auth.logout = logout;
         auth.signup = signup;
 
         function login ( username, password ) {
-            'use strict';
 
-            var user = {
-              username : username,
-              password : password
-            };
-            AuthFactory
-              .login(user)
-              .then(function (res) {
-                  console.log('Response is', res);
-                  console.log('Token is', res.data.token);
-              }, handleError)
-              .then(function() {
-                $state.go('dashboard');
-              });
+          var user = {
+            username : username,
+            password : password
+          };
+          AuthFactory
+            .login(user)
+            .then(function (res) {
+                console.log('Response is', res);
+                console.log('Token is', res.data.token);
+            }, handleError)
+            .then(function() {
+              $state.go('dashboard');
+            });
         }
 
         function logout () {
-            AuthFactory.logout();
+          AuthFactory.logout();
         }
 
         function signup () {
-          AuthFactory.signup();
+          AuthFactory.signup(auth);
         }
 
         function handleError (err) {
-            console.log('Err is ', err);
+          console.log('Err is ', err);
         }
+
     });
 })();
--- public/app/controllers/dashboard.js
+++ public/app/controllers/dashboard.js
@@ -1,5 +1,23 @@
-angular.module('locus.dashboard', [])
+(function () {
+	'use strict';
 
-.controller('DashboardCtrl', function ( $scope ) {
+	angular
+		.module('locus.dashboard', [])
+		.controller('DashboardCtrl', DashboardCtrl);
 
-});
+	DashboardCtrl.$inject = ['AuthFactory', '$state'];
+
+	function DashboardCtrl(AuthFactory, $state) {
+		var vm = this;
+		vm.title = 'DashboardCtrl';
+		vm.logout = logout;
+
+		function logout() {
+			AuthFactory.logout();
+			$state.go('login');
+		};
+
+	}
+
+})();
+
